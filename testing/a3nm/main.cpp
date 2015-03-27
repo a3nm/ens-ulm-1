fc9aa0596af5e9ad0884b104877453f482df7d12
Antoine Amarilli <a3nm@a3nm.net>
2015-03-27 20:19:54 +0100

    testing

---
+++ testing/a3nm/main.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <map>
+#include <time.h>
+
+using namespace std;
+
+#define MAXN 1002
+
+int main(int argc, char **argv) {
+  scanf("%d", &R);
+  scanf("%d", &S);
+  scanf("%d", &U);
+  scanf("%d", &P);
+  scanf("%d", &M);
+
+  // TODO
+
+  return 0;
+}
+
