cbeb07cbe07c28eaf5bd85e881e13cc28a6a54e4
Antoine Amarilli <a3nm@a3nm.net>
2015-03-28 10:53:08 +0100

    contest

---
+++ contest/a3nm/main.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <cstdio>
+#include <algorithm>
+#include <map>
+#include <time.h>
+
+using namespace std;
+
+#define MAXN 200
+
+int R, C, H, S;
+char pizz[MAXN][MAXN]; // row, col
+
+vector<int> size;
+vector<vector<int> > G;
+vector<int> r1;
+vector<int> c1;
+vector<int> r2;
+vector<int> c2;
+vector<int> ham;
+
+int main(int argc, char **argv) {
+  scanf("%d%d%d%d", &R, &C, &H, &S);
+  
+  return 0;
+}
+
+
