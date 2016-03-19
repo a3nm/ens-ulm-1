8a9b8a526383a32b811d4ca7d0af0cfc097911ed
Antoine Amarilli <a3nm@a3nm.net>
2016-03-19 15:03:29 +0100

    compile

--- 2016/contest/compile.sh
+++ 2016/contest/compile.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+EXEC=${1:-./a.out}
+
+tar zcf source.tgz *.sh *.cpp *.cc
 g++ -std=c++14 main.cpp -Wall -O2
 for i in *.in ;
 do
     echo "Doing $i " ;
-    ./a.out < $i > ${i%%in}out ;
+    $EXEC < $i > ${i%%in}out ;
 done ;
